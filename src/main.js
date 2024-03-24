0ae0766c43975637053c0612a9881c551169ae87
Ziad <Ziadyaser21@gmail.com>
2024-03-24 03:07:11 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -14,3 +14,58 @@ closeIcon.addEventListener( "click", () => {
 hamburger.addEventListener("click", () =>{
     navMenu.classList.remove('hidden');
 })
+
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item__wrap");
+const food = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snacks");
+const beverage = document.querySelectorAll(".beverage");
+
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(tab => {
+            tab.classList.remove("active")
+        })
+        tab.classList.add("active")
+
+        const tabval = tab.getAttribute('data-tabs')
+
+        all.forEach(item => {
+            item.style.display = 'none'
+        })
+
+        if (tabval == 'food') {
+            food.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else if (tabval == 'snacks'){
+            snacks.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else if (tabval == 'beverage'){
+            beverage.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else {
+            all.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+    })
+})        
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
